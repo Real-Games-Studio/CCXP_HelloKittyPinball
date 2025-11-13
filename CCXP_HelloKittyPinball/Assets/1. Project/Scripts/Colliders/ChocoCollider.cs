7febd80c32a9e1cc2d2b16149fd07cfa0918a5e2
Otavio Ribeiro <38363869+PandaCubGames@users.noreply.github.com>
2025-11-13 18:21:04 -0300

    Add ChocoCollider script to manage player interactions with chocolate areas

---
+++ CCXP_HelloKittyPinball/Assets/1. Project/Scripts/Colliders/ChocoCollider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace _1._Project.Scripts.Colliders
+{
+	public class ChocoCollider : MonoBehaviour
+	{
+		public GameObject ChocoOutPosition;
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.CompareTag("Player"))
+			{
+				other.transform.position = ChocoOutPosition.transform.position;
+			}
+		}
+	}
+}
