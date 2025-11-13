7ffc4fc3a442b8f971f44f3400590949ea060def
Otavio Ribeiro <38363869+PandaCubGames@users.noreply.github.com>
2025-11-13 18:42:39 -0300

    Add HelloKittyCollider script to manage player interactions and ball release timing

---
+++ CCXP_HelloKittyPinball/Assets/1. Project/Scripts/Colliders/HelloKittyCollider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace _1._Project.Scripts.Colliders
+{
+	public class HelloKittyCollider : MonoBehaviour
+	{
+		public GameObject HelloKittyOutPosition;
+		public float TimeWaiting;
+		private GameObject _currentBall;
+		
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.CompareTag("Player"))
+			{
+				_currentBall = other.gameObject;
+				StartCoroutine(WaitToRelease());
+			}
+		}
+		
+		private IEnumerator WaitToRelease()
+		{
+			_currentBall.SetActive(false);
+			yield return new WaitForSeconds(TimeWaiting);
+			_currentBall.transform.position = HelloKittyOutPosition.transform.position;
+			_currentBall.SetActive(true);
+		}
+	}
+}
