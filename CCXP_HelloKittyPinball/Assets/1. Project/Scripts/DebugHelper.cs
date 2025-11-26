3a6cd4e0c6f2f657ebf8d5aa846317d3554d5d2b
Otavio Ribeiro <38363869+PandaCubGames@users.noreply.github.com>
2025-11-26 18:56:46 -0300

    Refactor DebugHelper to use a list of DebugObjects for key-based position movement

--- CCXP_HelloKittyPinball/Assets/1. Project/Scripts/DebugHelper.cs
+++ CCXP_HelloKittyPinball/Assets/1. Project/Scripts/DebugHelper.cs
@@ -1,62 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace _1._Project.Scripts
+namespace _1._Project.Scripts{
+
+	[Serializable]
+public class DebugObject
 {
-	public class DebugHelper : MonoBehaviour
+	public Transform Position;
+	public KeyCode Key;
+}
+
+public class DebugHelper : MonoBehaviour
 	{
 		public GameManager GameManager;
-		public Transform PositionChoco;
-		public KeyCode ChocoKey;
-		public Transform PositionHelloKitty;
-		public KeyCode HelloKittyKey;
-		public Transform PositionKeroppi;
-		public KeyCode KeroppiKey;
-		public Transform PositionPotchaco;
-		public KeyCode PotchacoKey;
-		public Transform PositionCinammon;
-		public KeyCode CinammonKey;
-		public Transform PositionMaru;
-		public KeyCode MaruKey;
-		public Transform PositionMyMelody;
-		public KeyCode MyMelodyKey;
-		public Transform PositionKuromi;
-		public KeyCode KuromiKey;
-
+		public List<DebugObject> DebugObjects;
 		public void Update()
 		{
-			if (Input.GetKey(ChocoKey))
+			foreach (var debugObject in DebugObjects)
 			{
-				MoveBallToPosition(PositionChoco);
-			}
-			if (Input.GetKey(HelloKittyKey))
-			{
-				MoveBallToPosition(PositionHelloKitty);
-			}
-
-			if (Input.GetKey(KeroppiKey))
-			{
-				MoveBallToPosition(PositionKeroppi);
-			}
-			if (Input.GetKey(PotchacoKey))
-			{
-				MoveBallToPosition(PositionPotchaco);
-			}
-			if (Input.GetKey(CinammonKey))
-			{
-				MoveBallToPosition(PositionCinammon);
-			}
-			if (Input.GetKey(MaruKey))
-			{
-				MoveBallToPosition(PositionMaru);
-			}
-			if (Input.GetKey(MyMelodyKey))
-			{
-				MoveBallToPosition(PositionMyMelody);
-			}
-			if (Input.GetKey(KuromiKey))
-			{
-				MoveBallToPosition(PositionKuromi);
+				if (Input.GetKey(debugObject.Key))
+				{
+					MoveBallToPosition(debugObject.Position);
+				}
 			}
 		}
 		
