019f0eae22f88f0c911f4c8c1c2ec0c87ebdafe1
Otavio Ribeiro <38363869+PandaCubGames@users.noreply.github.com>
2025-11-26 18:36:10 -0300

    Add new character positions and update particle colors

--- CCXP_HelloKittyPinball/Assets/1. Project/Scripts/DebugHelper.cs
+++ CCXP_HelloKittyPinball/Assets/1. Project/Scripts/DebugHelper.cs
@@ -14,6 +14,14 @@ namespace _1._Project.Scripts
 		public KeyCode KeroppiKey;
 		public Transform PositionPotchaco;
 		public KeyCode PotchacoKey;
+		public Transform PositionCinammon;
+		public KeyCode CinammonKey;
+		public Transform PositionMaru;
+		public KeyCode MaruKey;
+		public Transform PositionMyMelody;
+		public KeyCode MyMelodyKey;
+		public Transform PositionKuromi;
+		public KeyCode KuromiKey;
 
 		public void Update()
 		{
@@ -33,6 +41,22 @@ namespace _1._Project.Scripts
 			if (Input.GetKey(PotchacoKey))
 			{
 				MoveBallToPosition(PositionPotchaco);
+			}
+			if (Input.GetKey(CinammonKey))
+			{
+				MoveBallToPosition(PositionCinammon);
+			}
+			if (Input.GetKey(MaruKey))
+			{
+				MoveBallToPosition(PositionMaru);
+			}
+			if (Input.GetKey(MyMelodyKey))
+			{
+				MoveBallToPosition(PositionMyMelody);
+			}
+			if (Input.GetKey(KuromiKey))
+			{
+				MoveBallToPosition(PositionKuromi);
 			}
 		}
 		
