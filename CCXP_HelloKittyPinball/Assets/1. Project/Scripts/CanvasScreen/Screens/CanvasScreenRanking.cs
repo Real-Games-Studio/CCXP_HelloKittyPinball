c66b2eecf4f21c5a0beeae9fe56cd3d257db1629
Otavio Ribeiro <38363869+PandaCubGames@users.noreply.github.com>
2025-11-27 17:20:10 -0300

    Update materials and prefab configurations for Ball and M_Ball; adjust rendering properties and add new sprites

--- CCXP_HelloKittyPinball/Assets/1. Project/Scripts/CanvasScreen/Screens/CanvasScreenRanking.cs
+++ CCXP_HelloKittyPinball/Assets/1. Project/Scripts/CanvasScreen/Screens/CanvasScreenRanking.cs
@@ -91,7 +91,7 @@ public class CanvasScreenRanking : CanvasScreen
             int? position = ScoreManager.Instance.GetLastRecordedScorePosition(displayDaylyRanking);
             SetMatchPosition(position);
             SetMatchScore(lastScore.Value.score);
-            if (position>=3)
+            if (position<=3)
             {
                 
                 if (matchScoreText == null)
@@ -111,7 +111,7 @@ public class CanvasScreenRanking : CanvasScreen
                 ? ScoreManager.Instance.GetPositionInRankingOnCurrentDay(currentScore)
                 : ScoreManager.Instance.GetPositionInRanking(currentScore);
             
-            if (position>=3)
+            if (position<=3)
             {
                 
                 if (matchScoreText == null)
